279c0ba0d07fef41f3b4c779de7941877a47d7bd
zouwx2cs <wxzou@mail.bnu.edu.cn>
2018-12-20 18:36:55 +0800

    Correct Solution5.cpp

--- Solutions/001.二维数组中的查找/Solution5.cpp
+++ Solutions/001.二维数组中的查找/Solution5.cpp
@@ -1,9 +1,41 @@
 class Solution {
+private:
+    bool FindInBlock(int target, vector<vector<int> > &array,
+                     int leftUpRow, int leftUpColumn,
+                     int rightDownRow, int rightDownColumn)
+    {
+        if (leftUpColumn >= rightDownColumn
+            || leftUpRow >= rightDownRow)
+            return false ;
+
+        const int midColumn = (leftUpColumn + rightDownColumn)>>1 ;
+        const int midRow = (leftUpRow + rightDownRow)>>1 ;
+        const int midValue = array[midRow][midColumn] ;
+
+        if (target < midValue)
+        {
+            if ( FindInBlock(target, array, leftUpRow, leftUpColumn, midRow, midColumn)
+                || FindInBlock(target, array, midRow, leftUpColumn, rightDownRow, midColumn)
+                || FindInBlock(target, array, leftUpRow, midColumn, midRow, rightDownColumn) )
+                return true ;
+            return false ;
+        }
+        else if ( target > midValue )
+        {
+            if ( FindInBlock(target, array, midRow+1, midColumn+1, rightDownRow, rightDownColumn)
+                || FindInBlock(target, array, midRow, midColumn+1, midRow+1, rightDownColumn)
+                || FindInBlock(target, array, midRow+1, midColumn, rightDownRow, midColumn+1) )
+                return true ;
+            return false ;
+        }
+        else
+            return true ;
+    }
 public:
     bool Find(int target, vector<vector<int> > array) {
-        unordered_set<int> s ;
-        for (auto v: array)
-            s.insert(v.begin(), v.end()) ;
-        return s.find(target) != s.end() ;
+        if (0 == array.size())
+            return false ;
+
+        return FindInBlock(target, array, 0, 0, array.size(), array.at(0).size()) ;
     }
 } ;
