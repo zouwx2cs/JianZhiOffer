ea1e7a978a701c55c2cea6714dd573fb97158a59
Bless <1041782129@qq.com>
2018-12-27 19:23:12 +0800

    Solution10.java

---
+++ Solutions/010.矩形覆盖/Solution10.java
@@ -0,0 +1,16 @@
+//所用时间545ms
+//所占内存9312k
+public class Solution {
+    public int RectCover(int target) {
+        if (target == 0) {
+            return 0;
+        }
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        return RectCover(target - 1) + RectCover(target - 2);
+    }
+}
