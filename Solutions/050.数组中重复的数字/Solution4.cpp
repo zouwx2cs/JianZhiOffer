e2e43695924a5d6018e3800378c24e645e4a949f
zouwx2cs <zouwx2cs@gmail.com>
2019-01-03 22:20:23 +0800

    add 050 cpp version(exchange)

---
+++ Solutions/050.数组中重复的数字/Solution4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // Parameters:
+    //        numbers:     an array of integers
+    //        length:      the length of array numbers
+    //        duplication: (Output) the duplicated number in the array number
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    bool duplicate(int numbers[], int length, int* duplication) {
+        for (int i = 0; i < length; ++i)
+        {
+            while (numbers[i] != i)
+            {
+                if (numbers[numbers[i]] == numbers[i])
+                {
+                    *duplication = numbers[i] ;
+                    return true ;
+                }
+                swap(numbers[i], numbers[numbers[i]]) ;
+            }
+        }
+        
+        return false ;
+    }
+};
