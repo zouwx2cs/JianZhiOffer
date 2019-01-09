4e3eb6e7c6fba57889ff0062667d94f9d3415640
Bless <1041782129@qq.com>
2019-01-09 20:33:16 +0800

    Solution18.java

---
+++ Solutions/018.二叉树的镜像/Solution18.java
@@ -0,0 +1,31 @@
+//23ms
+//9620k
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public void Mirror(TreeNode root) {
+        TreeNode temp;
+        if (root != null) {
+            temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            if (root.left != null) {
+                Mirror(root.left);
+            }
+            if (root.right != null) {
+                Mirror(root.right);
+            }
+        }
+    }
+}
